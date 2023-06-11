d4c062e8f5161367633eeff2e2f91c287157ec14
Victor Torres <victor.fmt@gmail.com>
2023-06-11 00:06:53 -0400

    submit answer

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const e = "birthday";
+
+function writeCards(names, event) {
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        cards.push(message);
+    };
+    return cards;
+};
+
+function countDown(number) {
+    for (let i = number; i > -1; i--) {
+        console.log(i) ;
+    };  
+};
